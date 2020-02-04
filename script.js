df700eb64f53fa9dd506e6f537c39733ed786e90
Francesco Pandolfi <francesco.pan@gmail.com>
2020-02-04 23:08:39 +0100

    Animated gallery

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+$(document).on('click', '.pic', function() {
+  $('.pic').toggleClass('away');
+  $(this).removeClass('away').toggleClass('active');
+});
+
+
+
+
+});
